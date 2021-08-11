085aa4d733d617ea23d52c256a5829dc60b7ca61
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-08-10 23:57:59 -0300

    Finalizando exercícios de Call back
    Finalizando os exercicios de call back da aula 10

--- quinzena2/funcoes-de-array/index.js
+++ quinzena2/funcoes-de-array/index.js
@@ -52,4 +52,54 @@ function gerarMensagemDesconto(item, posicao, array){
  const petsPoodle = pets.map(gerarMensagemDesconto);
  console.log(petsPoodle);
 
+//exercicio 2
+const produtos = [
+   { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+   { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+   { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+   { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+   { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+   { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+   { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+   { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+   { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+   { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+];
 
+//2-a)
+const apenasNomeDosProdutos = produtos.map((item, index, array) => {
+   return item.nome
+})
+
+console.log(apenasNomeDosProdutos);
+
+//2-b)
+let listaDedesconto = produtos.map((produto) => {
+   return {
+       nome: produto.nome, 
+       preco: (produto.preco-(produto.preco*0.05))
+   }
+});
+
+console.log(listaDedesconto)
+
+//2c)
+const apenasProdutosDeBeber = produtos.filter((item, index, array) => {
+   return item.categoria == "Bebidas"
+});
+
+console.log(apenasProdutosDeBeber);
+
+//2-d)
+const apenasProdutosYpe = produtos.filter((item, index, array) => {
+   return item.nome.includes("Ypê")
+});
+console.log(apenasProdutosYpe)
+
+//2-e)
+function gerarMensagem(produtos){
+   return `Compre ${produtos.nome} por ${produtos.preco}`
+}
+
+const frase = apenasProdutosYpe.map(gerarMensagem);
+console.log(frase)
