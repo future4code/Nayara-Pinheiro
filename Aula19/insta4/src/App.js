530279786c0678ecfaa91faaec12d0581e2571d4
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-09-16 23:04:12 -0300

    implementado o Array para os posts

--- Aula19/insta4/src/App.js
+++ Aula19/insta4/src/App.js
@@ -9,25 +9,46 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+
 class App extends React.Component {
+  state = {
+    postagem: [
+      {
+        nomeUsuario:'Nayara',
+        fotoUsuario:'https://picsum.photos/50/50',
+        fotoPost:'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: 'Dara',
+        fotoUsuario:'https://picsum.photos/id/237/200/300',
+        fotoPost:'https://picsum.photos/seed/picsum/200/300'
+      },
+      {
+        nomeUsuario:'Lívia',
+        fotoUsuario: 'https://picsum.photos/200/300?grayscale',
+        fotoPost: 'https://picsum.photos/200/300/?blur=2'
+      }
+
+    ]
+  }
+
+
+
   render() {
+
+    const listaDePostagem = this.state.postagem.map((postagem) => {
+        return (
+          <Post
+          nomeUsuario={postagem.nomeUsuario}
+          fotoUsuario= {postagem.fotoUsuario}
+          fotoPost={postagem.fotoUsuario}
+        />
+        );
+    });
+
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'Nayara'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Dara'}
-          fotoUsuario={'https://picsum.photos/id/237/200/300'}
-          fotoPost={'https://picsum.photos/seed/picsum/200/300'}
-        />
-        <Post
-          nomeUsuario={'Lívia'}
-          fotoUsuario={'https://picsum.photos/200/300?grayscale'}
-          fotoPost={'https://picsum.photos/200/300/?blur=2'}
-        />
+       {listaDePostagem}
       </MainContainer>
     );
   }
