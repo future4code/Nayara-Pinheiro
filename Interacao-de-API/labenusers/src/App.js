4d560866133886e2a4f9d19175e6e5229140daae
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-10-13 21:31:08 -0300

    Criando state na Class App

--- Interacao-de-API/labenusers/src/App.js
+++ Interacao-de-API/labenusers/src/App.js
@@ -1,15 +1,29 @@
 import React from "react"; 
 import Labenusers from "./Paginas/Labenusers";
+import ListasDeUsuarios from "./Paginas/ListasUsusarios";
+
+export default class App extends React.Component{
+  state = {
+    telaAtual:"cadastro"
+  }
+
+  escolheTela = () => {
+    switch (this.state.telaAtual){
+      case "cadastro":
+        return <Labenusers/>
+      case "lista":
+        return <ListasDeUsuarios/>
+      default:
+        return <div>Erro! Página Não Encontrada!</div>
+    }
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Labenusers/>
-      </header>
-    </div>
-  );
+  render(){
+    return(
+      <div>
+        {this.escolheTela}
+      </div>
+    )
+  }
 }
-
-export default App;
