1950c59033aff55f869202de2a54105109c213db
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-10-20 00:06:48 -0300

    Alterado a caixa de comentário

--- useState InstaLab 2/insta-lab-hooks/src/components/Post/Post.js
+++ useState InstaLab 2/insta-lab-hooks/src/components/Post/Post.js
@@ -26,12 +26,25 @@ const Post = (props) => {
   };
 
   const onClickComentario = () => {
+    setComentando(!comentando)
   };
 
   const enviarComentario = (comentario) => {
   }
 
   const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
+
+  const caixaDeComentario = comentando ? (
+       <SecaoComentario enviarComentario={enviarComentario}/>
+    ) : (
+      comentarios.map(comentario => {
+        return (
+          <CommentContainer>
+            <p>{comentario}</p>
+          </CommentContainer>
+        )
+      })
+  )
 
 
   return (
@@ -56,7 +69,7 @@ const Post = (props) => {
           // valorContador={numeroComentarios}
         />
       </PostFooter>
-      {/* {caixaDeComentario} */}
+      {caixaDeComentario}
     </PostContainer>
   )
 }
