5ecf9b893a12c15dd9db42d1193d089b05efc48b
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-10-22 01:01:01 -0300

    Adicionado useEffect no App.js e criado pokeCard.js

--- Aula 29/pokecard/src/App.js
+++ Aula 29/pokecard/src/App.js
@@ -1,9 +1,38 @@
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PokeCard from "./componentes/pokeCard";
 
 function App() {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, SetPokeName] = useState("");
+
+  changePokeName = event => {
+    SetPokeName(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+    .then((response) => {
+      setPokeList(response.data.results)
+    })
+    .catch((err) => {
+      alert(err.reponse.data);
+    });
+  },[pokeName])
+
   return (
     <div>
-     <h1>Eu te amo meu amor</h1>
+     <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+         {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+        {pokeName && <PokeCard pokemon={pokeName} />}
     </div>
   );
 }
